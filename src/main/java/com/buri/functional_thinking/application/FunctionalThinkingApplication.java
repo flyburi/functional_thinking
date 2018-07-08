e5f5e691fd16fffab6ff75e258dbd601c67c9f4b
tina.s <tina.s@kakaocorp.com>
2018-07-08 22:42:53 +0900

    git init

---
+++ src/main/java/com/buri/functional_thinking/application/FunctionalThinkingApplication.java
@@ -0,0 +1,12 @@
+package com.buri.functional_thinking.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalThinkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalThinkingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/buri/functional_thinking/FunctionalThinkingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.buri.functional_thinking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FunctionalThinkingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
