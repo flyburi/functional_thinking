945d28d0a9831a8069cdb371b55c0089567019a1
tina.s <tina.s@kakaocorp.com>
2018-07-08 22:43:18 +0900

    chapter1 - find word frequency

---
+++ src/main/java/com/buri/functional_thinking/application/chapter1/Words.java
@@ -0,0 +1,54 @@
+package com.buri.functional_thinking.application.chapter1;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Words {
+
+	private Set<String> NON_WORDS = new HashSet<String>() {
+		{
+			add(",");
+		}
+	};
+
+	public Map freq(String words) {
+		TreeMap<String, Integer> wordMap = new TreeMap<>();
+
+		Matcher m = Pattern.compile("\\w+").matcher(words);
+
+		while (m.find()) {
+			String word = m.group().toLowerCase();
+			if (!NON_WORDS.contains(word)) {
+				if (wordMap.get(word) == null) {
+					wordMap.put(word, 1);
+				} else {
+					wordMap.put(word, wordMap.get(word) + 1);
+				}
+			}
+		}
+
+		return wordMap;
+	}
+
+	public Map freqLambda(String words) {
+		TreeMap<String, Integer> wordMap = new TreeMap<>();
+
+		regexForList(words, "\\w+").stream()
+				.map(w -> w.toLowerCase())
+				.filter(w -> !NON_WORDS.contains(w))
+				.forEach(w -> wordMap.put(w, wordMap.getOrDefault(w, 0) + 1));
+		return wordMap;
+	}
+
+	private List<String> regexForList(String words, String regex) {
+		List wordList = new ArrayList();
+		Matcher m = Pattern.compile(regex).matcher(words);
+		while (m.find()) {
+			wordList.add(m.group());
+		}
+
+		return wordList;
+	}
+
+}
---
+++ src/test/java/com/buri/functional_thinking/chapter1/WordsTest.java
@@ -0,0 +1,32 @@
+package com.buri.functional_thinking.chapter1;
+
+
+import com.buri.functional_thinking.application.chapter1.Words;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Map;
+
+@Slf4j
+public class WordsTest {
+
+	private static String words = "java, scala, kotlin, swift, java, scala, swift, java";
+
+	@Test
+	public void freq() {
+		Words word = new Words();
+		Map<String, Integer> map = word.freq(words);
+
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			log.info("Key : " + entry.getKey() + " , Count : " + entry.getValue());
+		}
+	}
+
+	@Test
+	public void freqLambda() {
+		Words word = new Words();
+		Map<String, Integer> map = word.freqLambda(words);
+		map.forEach((k, v) -> log.info("Key : " + k + " , Count : " + v));
+
+	}
+}
